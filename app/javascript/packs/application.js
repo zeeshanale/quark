4a8d098e8c6ccf19e927482b555840a63e349303
zeeshanale <zeeshan.alec1@gmail.com>
2022-10-22 12:26:28 +0500

    Basic working done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,4 +16,7 @@ ActiveStorage.start()
 
 $(document).on('turbolinks:load',function(){
     $('.ui.dropdown').dropdown();
+    $('.message .close').on('click', function() {
+        $(this).closest('.message').transition('fade');
+    });
 })
